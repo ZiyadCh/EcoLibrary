24697550c48d86d4537e8542cd9186ec212545c1
ZiyadCh <chetoziyad@gmail.com>
2026-03-09 13:24:45 +0000

    added models and relations

---
+++ app/Models/Categorie.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    //
+    public function livres()
+    {
+        return $this->hasMany(Livre::class);
+    }
+}
